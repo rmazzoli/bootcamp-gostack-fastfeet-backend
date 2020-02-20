8c38e0a09b906e7ffa22c07c52f11d74504fd5d5
rmazzoli <rafael.mazzoli@hotmail.com>
2020-02-19 23:22:02 -0300

    chore create seed admin user

--- src/database/migrations/20200220021423-users.js
+++ src/database/migrations/20200220021423-users.js
@@ -1,38 +1,39 @@
 module.exports = {
-  up: (queryInterface, Sequelize) => queryInterface.createTable('users', {
-    id: {
-      type: Sequelize.INTEGER,
-      allowNull: true,
-      autoIncrement: true,
-      primaryKey: true,
-    },
-    name: {
-      type: Sequelize.STRING,
-      allowNull: true,
-    },
-    email: {
-      type: Sequelize.STRING,
-      allowNull: true,
-      unique: true,
-    },
-    password_hash: {
-      type: Sequelize.STRING,
-      allowNull: true,
-    },
-    admin: {
-      type: Sequelize.BOOLEAN,
-      defaultValue: false,
-      allowNull: true,
-    },
-    created_at: {
-      type: Sequelize.DATE,
-      allowNull: true,
-    },
-    updated_at: {
-      type: Sequelize.DATE,
-      allowNull: true,
-    },
-  }),
+  up: (queryInterface, Sequelize) =>
+    queryInterface.createTable('users', {
+      id: {
+        type: Sequelize.INTEGER,
+        allowNull: true,
+        autoIncrement: true,
+        primaryKey: true,
+      },
+      name: {
+        type: Sequelize.STRING,
+        allowNull: true,
+      },
+      email: {
+        type: Sequelize.STRING,
+        allowNull: true,
+        unique: true,
+      },
+      password_hash: {
+        type: Sequelize.STRING,
+        allowNull: true,
+      },
+      admin: {
+        type: Sequelize.BOOLEAN,
+        defaultValue: false,
+        allowNull: true,
+      },
+      created_at: {
+        type: Sequelize.DATE,
+        allowNull: true,
+      },
+      updated_at: {
+        type: Sequelize.DATE,
+        allowNull: true,
+      },
+    }),
 
-  down: (queryInterface) => queryInterface.dropTable('users'),
+  down: queryInterface => queryInterface.dropTable('users'),
 };
